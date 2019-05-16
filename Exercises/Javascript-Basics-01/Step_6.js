8821efb9c0f3c72896e6e9d0127780592866d613
BringSteam <>
2019-05-16 22:58:57 +0300

    JS Basics 01 Step 6: External Files and Simple Math

---
+++ Exercises/Javascript-Basics-01/Step_6.js
@@ -0,0 +1,6 @@
+var firstNb = prompt('Please enter the 1st number:');
+var secondNb = prompt('Please enter the 2nd number:');
+
+var remainder = firstNb % secondNb;
+
+alert('The remainder of the division is: ' + remainder);
