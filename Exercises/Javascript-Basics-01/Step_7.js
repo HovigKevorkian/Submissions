4e3026ab3f4dfabb958dc12748a47fbc36f34a03
BringSteam <>
2019-05-16 23:11:10 +0300

    JS Basics 01 Step 7: Multiple Operations

---
+++ Exercises/Javascript-Basics-01/Step_7.js
@@ -0,0 +1,6 @@
+var shoeSize = prompt('Please enter your shoe size:');
+var birthYear = prompt('Please enter your year of Birth:');
+
+var result = (((((shoeSize * 2) + 5) * 50) - birthYear) + 1766);
+
+alert(result);
