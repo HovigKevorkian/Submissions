29e79bcdba3ef4d0f357b2117af7360b846093ad
Hovik K. kevorkian <hovikkevork@gmail.com>
2019-05-20 08:06:11 +0300

    js step1. no.1 and 2

---
+++ Exercises/Javascript-Basics-01/Step_4.html + Step_4.js
@@ -0,0 +1,4 @@
+var firstname = prompt("Enter name")
+var surname = prompt("Enter surname")
+var city = prompt("Enter city")
+alert( "Hello " + firstname + " " +  surname +  " from "  + city )
---
+++ Exercises/Javascript-Basics-01/Step_5.html + Step_5.js
@@ -0,0 +1,5 @@
+var FirstNumber= parseInt(prompt("Enter first number"))
+var SecondNumber = parseInt(prompt("Enter second number"))
+alert( FirstNumber * SecondNumber )
+
+
