6caacf290762f40a4a9c4163fde5644e8a9f3a85
Hovik K. kevorkian <hovikkevork@gmail.com>
2019-05-20 08:13:06 +0300

    Step_6.html + Step_6.js

---
+++ Exercises/Javascript-Basics-01/Step_6.html + Step_6.js
@@ -0,0 +1,6 @@
+var FirstNumber= parseInt(prompt("Enter first number"))
+var SecondNumber = parseInt(prompt("Enter second number"))
+var divide =  FirstNumber / SecondNumber 
+alert( FirstNumber % SecondNumber)
+
+
