fae3dcbc68ee95299a52908c60fead4d670bd5d5
BringSteam <>
2019-05-23 10:27:31 +0300

    JS Basics 01 Step 4 v.1.0.2

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name = prompt('Please enter your name:');
+var surname = prompt('Please enter your surname:');
+var city = prompt('Please enter your city:');
+
+alert('Hello, ' + name + ' ' + surname + ' from ' + city);
