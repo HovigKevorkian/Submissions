1e205a834572cf21eb746bb92c76325888af60cc
BringSteam <>
2019-06-11 11:37:27 +0300

    Step 2 - Create a simple express server

---
+++ Challenge/movie-database/db/db.js
@@ -0,0 +1,10 @@
+const movies = [
+  {
+    id: 1,
+    title: 'Men in Black: International (2019)',
+    description:
+      'The Men in Black have always protected the Earth from the scum of the universe. In this new adventure, they tackle their biggest threat to date: a mole in the Men in Black organization.'
+  }
+];
+
+export default movies;
