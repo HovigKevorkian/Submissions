cc5d8d6eae4437f33ad12acfd4da025c272fff3f
Hovik K. kevorkian <hovikkevork@gmail.com>
2019-06-12 12:08:28 +0300

    step 2

--- Challenge/movie-database/Server.js
+++ Challenge/movie-database/Server.js
@@ -1,1 +1,20 @@
- 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', function(req, res){
+    // console.log(req)
+    // console.log(res)
+    res.send({message:"ok", status:"yayyyy", id:1});
+})
+
+app.get('/hello/:x', function(req, res){
+    // console.log(req)
+    // console.log(res)
+    const n = req.query.name;
+    res.send({url:req.url, query:n, paramater:req.params.x});
+})
+
+app.listen(port, function () {
+    console.log("Server is running on" + port + "port");
+});
