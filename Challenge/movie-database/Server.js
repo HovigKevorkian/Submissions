d5a4aa77fed9918155929cbb5eb08274df51b10e
Hovik K. kevorkian <hovikkevork@gmail.com>
2019-06-12 13:58:47 +0300

    Step3

--- Challenge/movie-database/Server.js
+++ Challenge/movie-database/Server.js
@@ -2,19 +2,39 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', function(req, res){
+ app.get('/', function(req, res){
     // console.log(req)
     // console.log(res)
-    res.send({message:"ok", status:"yayyyy", id:1});
-})
+     res.send({message:"ok", status:"yayyyy", id:1});
+ })
 
-app.get('/hello/:x', function(req, res){
+ app.get('/hello/x:', function(req, res){
     // console.log(req)
     // console.log(res)
-    const n = req.query.name;
-    res.send({url:req.url, query:n, paramater:req.params.x});
-})
+     const n = req.query.name;
+     res.send({url:req.url, query:n, paramater:req.params.x});
+ })
 
-app.listen(port, function () {
-    console.log("Server is running on" + port + "port");
-});
+ app.get('/test', function(req, res){
+    // console.log(req)
+    // console.log(res)
+    
+     res.send({status:200, message:"ok"});
+ })
+
+ app.get('/time', function(req,res) {
+    //  const message = new Date();
+    //  message.get.hours();
+    //  amessage.get.minutes();
+    //  message.get.seconds();
+     var datetime = new Date();
+     var hour = datetime.getHours();
+     var minutes = datetime.getMinutes();
+     var seconds = datetime.getSeconds()
+     res.send({status:200, message: "Current Time is " + hour + ":" + minutes + ":" + seconds });
+    
+ });
+
+ app.listen(port, function(){
+     console.log("Server is running on " + port + " port");
+ })
